c5ae58753bc066f7519fbd5a110f1a8d09417935
Nathan Voller <nathan.voller@me.com>
2019-11-15 15:41:30 +0000

    add fake journey data

---
+++ src/data/journeys.ts
@@ -0,0 +1,86 @@
+export const journeys = [
+  {
+    id: '1',
+    from: 'London',
+    to: 'Paris',
+    date: '2019-10-11',
+    steps: [
+      {
+        mode: 'bus',
+        from: 'Norwich',
+        to: 'Cambridge',
+      },
+      {
+        mode: 'train',
+        from: 'Cambridge',
+        to: 'London',
+      },
+      {
+        mode: 'plane',
+        from: 'London',
+        to: 'Amsterdam',
+      },
+      {
+        mode: 'taxi',
+        from: 'Amsterdam',
+        to: 'Hotel',
+      },
+    ]
+  },
+  {
+    id: '2',
+    from: 'Norwich',
+    to: 'Amsterdam',
+    date: '2019-10-11',
+    steps: [
+      {
+        mode: 'bus',
+        from: 'Norwich',
+        to: 'Cambridge',
+      },
+      {
+        mode: 'train',
+        from: 'Cambridge',
+        to: 'London',
+      },
+      {
+        mode: 'plane',
+        from: 'London',
+        to: 'Amsterdam',
+      },
+      {
+        mode: 'taxi',
+        from: 'Amsterdam',
+        to: 'Hotel',
+      },
+    ]
+  },
+  {
+    id: '3',
+    from: 'London',
+    to: 'Madrid',
+    date: '2019-10-11',
+    steps: [
+      {
+        mode: 'bus',
+        from: 'Norwich',
+        to: 'Cambridge',
+      },
+      {
+        mode: 'train',
+        from: 'Cambridge',
+        to: 'London',
+      },
+      {
+        mode: 'plane',
+        from: 'London',
+        to: 'Amsterdam',
+      },
+      {
+        mode: 'taxi',
+        from: 'Amsterdam',
+        to: 'Hotel',
+      },
+    ]
+  },
+];
