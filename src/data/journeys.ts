255e83de5fd81ce27fbbb3b4be1c0e3dd0edb92a
Nathan Voller <nathan.voller@me.com>
2019-11-15 20:34:00 +0000

    add types to journeys and refactor structure

--- src/data/journeys.ts
+++ src/data/journeys.ts
@@ -1,85 +1,90 @@
-export const journeys = [
+export interface Address {
+  street: string;
+  city: string;
+  postalcode: string;
+  country: string;
+  latitude: number;
+  longitude: number;
+}
+
+export interface JourneyStep {
+  mode: 'bus' | 'train' | 'plane' | 'taxi' | 'bicycle' | 'walk';
+  from: Address;
+  to: Address;
+  departTime: string;
+  arrivalTime: string;
+}
+
+export interface Journey {
+  id: string;
+  departTime: string;
+  from: Address;
+  to: Address;
+  steps: JourneyStep[];
+}
+
+export const journeys: Journey[] = [
   {
     id: '1',
-    from: 'London',
-    to: 'Paris',
-    date: '2019-10-11',
+    departTime: '2019-11-15',
+    from: {
+      street: 'Greenwich',
+      city: 'Greenwich',
+      country: 'United Kingdom',
+      postalcode: '',
+      latitude: 51.5287718,
+      longitude: -0.2416802,
+    },
+    to: {
+      street: 'France Street',
+      city: 'Paris',
+      country: 'United Kingdom',
+      postalcode: '',
+      latitude: 51.5287718,
+      longitude: -0.2416802,
+    },
     steps: [
       {
         mode: 'bus',
-        from: 'Norwich',
-        to: 'Cambridge',
+        from: {
+          street: 'Greenwich',
+          city: 'Greenwich',
+          country: 'United Kingdom',
+          postalcode: '',
+          latitude: 51.4874277,
+          longitude: -0.012965,
+        },
+        to: {
+          street: 'Victoria Station',
+          city: 'London',
+          country: 'United Kingdom',
+          postalcode: '',
+          latitude: 51.4952136,
+          longitude: -0.1460866,
+        },
+        departTime: '',
+        arrivalTime: '',
       },
       {
         mode: 'train',
-        from: 'Cambridge',
-        to: 'London',
-      },
-      {
-        mode: 'plane',
-        from: 'London',
-        to: 'Amsterdam',
-      },
-      {
-        mode: 'taxi',
-        from: 'Amsterdam',
-        to: 'Hotel',
-      },
-    ]
-  },
-  {
-    id: '2',
-    from: 'Norwich',
-    to: 'Amsterdam',
-    date: '2019-10-11',
-    steps: [
-      {
-        mode: 'bus',
-        from: 'Norwich',
-        to: 'Cambridge',
-      },
-      {
-        mode: 'train',
-        from: 'Cambridge',
-        to: 'London',
-      },
-      {
-        mode: 'plane',
-        from: 'London',
-        to: 'Amsterdam',
-      },
-      {
-        mode: 'taxi',
-        from: 'Amsterdam',
-        to: 'Hotel',
-      },
-    ]
-  },
-  {
-    id: '3',
-    from: 'London',
-    to: 'Madrid',
-    date: '2019-10-11',
-    steps: [
-      {
-        mode: 'bus',
-        from: 'Norwich',
-        to: 'Cambridge',
-      },
-      {
-        mode: 'train',
-        from: 'Cambridge',
-        to: 'London',
-      },
-      {
-        mode: 'plane',
-        from: 'London',
-        to: 'Amsterdam',
-      },
-      {
-        mode: 'taxi',
-        from: 'Amsterdam',
-        to: 'Hotel',
+        from: {
+          street: 'Victoria Station',
+          city: 'London',
+          country: 'United Kingdom',
+          postalcode: '',
+          latitude: 51.4952136,
+          longitude: -0.1460866,
+        },
+        to: {
+          street: 'France Street',
+          city: 'Paris',
+          country: 'United Kingdom',
+          postalcode: '',
+          latitude: 48.8589507,
+          longitude: 2.2770205,
+        },
+        departTime: '',
+        arrivalTime: '',
       },
     ]
   },
