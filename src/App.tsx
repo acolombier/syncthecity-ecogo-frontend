57f0fd2a0a49b80607ed9fed666097a34b759bde
Matthew Brookson <matthewbrookson@gmail.com>
2019-11-15 15:41:19 +0000

    Add search screen

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,7 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.scss';
+import './theme/index.scss';
 
 const App: React.FC = () => (
   <IonApp>
@@ -44,7 +45,11 @@ const App: React.FC = () => (
           <Route path="/journeys" component={Journeys} exact={true} />
           <Route path="/journeys/:id/detail" component={Details} />
           <Route path="/account" component={Account} />
-          <Route path="/" render={() => <Redirect to="/search" />} exact={true} />
+          <Route
+            path="/"
+            render={() => <Redirect to="/search" />}
+            exact={true}
+          />
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="search" href="/search">
