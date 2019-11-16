d76ad0a815c13db22b474a0477f1b297c33e8bce
Nathan Voller <nathan.voller@me.com>
2019-11-16 10:00:46 +0000

    add types for Journey Result search

--- src/services/searchResults.ts
+++ src/services/searchResults.ts
@@ -1,6 +1,57 @@
 import axios from 'axios';
 
-export const fetchSearchResults = async (to: string, from: string) => {
+export interface ArrivalPoint {
+  additionalProperties: any[];
+  commonName: string;
+  icsCode: string;
+  lat: number;
+  lon: number;
+  naptanId: string;
+  placeType: string;
+  platformName: string;
+  $type: string;
+}
+
+export interface DeparturePoint {
+  additionalProperties: any[];
+  commonName: string;
+  icsCode: string;
+  lat: number;
+  lon: number;
+  naptanId: string;
+  placeType: string;
+  platformName: string;
+  $type: string;
+}
+
+export interface StepInfo {
+  arrivalPoint: ArrivalPoint;
+  departurePoint: DeparturePoint;
+  distance: number;
+  type: string;
+}
+
+export interface Step {
+  arrivalDateTime: Date;
+  cost: number;
+  duration: number;
+  startDateTime: Date;
+  steps: StepInfo[];
+  type: string;
+}
+
+export interface JourneyResult {
+  arrivalCommonName: string;
+  arrivalDateTime: Date;
+  co2: number;
+  cost: number;
+  duration: number;
+  startCommonName: string;
+  startDateTime: Date;
+  steps: Step[];
+}
+
+export const fetchSearchResults = async (to: string, from: string): Promise<JourneyResult[]> => {
   const url = `http://192.168.10.207:8080/search`;
 
   const requestData = {
